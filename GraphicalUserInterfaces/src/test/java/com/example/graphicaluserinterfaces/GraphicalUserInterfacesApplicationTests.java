b8408938c14c0d372788931db70cb9f8ff05b5e4
tapordeimaia <146013799+tapordeimaia@users.noreply.github.com>
2025-01-09 11:31:57 +0200

    Add files via upload

---
+++ GraphicalUserInterfaces/src/test/java/com/example/graphicaluserinterfaces/GraphicalUserInterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphicaluserinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphicalUserInterfacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
